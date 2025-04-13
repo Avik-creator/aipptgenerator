f3009afe07c0036a7aefe6220f13838335cbb61c
Avik-creator <avikm744@gmail.com>
2025-04-13 15:35:58 +0530

    Added Responsive Design.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,48 @@
 import { Hero } from "@/components/page/Hero";
 import { PresentationForm } from "@/components/page/presentation-form";
 import { ThemeSelector } from "@/components/page/theme-selector";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "PPT CraftAI",
+  description: "Generate PowerPoint presentations using AI",
+  category: "Productivity",
+  openGraph:{
+    title: "PPT CraftAI",
+    description: "Generate PowerPoint presentations using AI",
+    url: "https://aipptgenerator.vercel.app",
+    siteName: "PPT CraftAI",
+    images: [
+      {
+        url: "/og-image.png",
+        width: 1200,
+        height: 630,
+      },
+    ],
+    locale: "en-US",
+  },
+  twitter:{
+    card: "summary_large_image",
+    title: "PPT CraftAI",
+    description: "Generate PowerPoint presentations using AI",
+    images: [
+      {
+        url: "/og-image.png",
+        width: 1200,
+        height: 630,
+      },
+    ],
+  },
+  icons: {
+    icon: "/favicon.ico",
+    shortcut: "/favicon.ico",
+    apple: "/apple-touch-icon.png",
+  },
+
+  
+
+
+};
 
 
 export default function Home() {
