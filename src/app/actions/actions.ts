5f583a25fa865cc1ee3cc9e929aa9659cb860125
Avik-creator <avikm744@gmail.com>
2025-04-16 21:19:58 +0530

    Added Changes.

--- src/app/actions/actions.ts
+++ src/app/actions/actions.ts
@@ -26,11 +26,6 @@ const formSchema = z.object({
     if (!response.ok) {
       return { error: "Failed to fetch client IP address" }
     }
-    // export function GET(request: Request) {
-    //   const ip = request.headers.get("x-client-ip") || request.headers.get("x-forwarded-for") || request.headers.get("cf-connecting-ip") || request.headers.get("x-real-ip") || request.headers.get("x-cluster-client-ip") || request.headers.get("x-client-ip") || request.headers.get("x-client-ip");
-    //   console.log("Client IP Address:", ip);
-    //   return new Response(JSON.stringify({ ip }))
-    // }
 
     const ipData = await response.json()
     const clientIPAddress = ipData.ip
--- src/app/api/ip/route.ts
+++ src/app/api/ip/route.ts
@@ -1,5 +1,5 @@
 export function GET(request: Request) {
-  const ip = request.headers.get("x-client-ip") || request.headers.get("x-forwarded-for") || request.headers.get("cf-connecting-ip") || request.headers.get("x-real-ip") || request.headers.get("x-cluster-client-ip") || request.headers.get("x-client-ip") || request.headers.get("x-client-ip");
+  const ip = request.headers.get("x-forwarded-for");
   console.log("Client IP Address:", ip);
   return new Response(JSON.stringify({ ip }))
 }
